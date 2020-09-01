4bbbf3b2ce35f0eb8238c02b951b155eccf2cfc3
Boky <bokysan@users.noreply.github.com>
2020-09-01 12:00:09 +0200

    Reduce the level of needed code
    Collapse case statements for the same level.
    
    Co-authored-by: Martin Helmich <kontakt@martin-helmich.de>

--- main.go
+++ main.go
@@ -36,9 +36,7 @@ func init() {
 		log.SetLevel(log.TraceLevel)
 	case "DEBUG":
 		log.SetLevel(log.DebugLevel)
-	case "WARN":
-		log.SetLevel(log.WarnLevel)
-	case "WARNING":
+	case "WARN", "WARNING":
 		log.SetLevel(log.WarnLevel)
 	case "ERROR":
 		log.SetLevel(log.ErrorLevel)
